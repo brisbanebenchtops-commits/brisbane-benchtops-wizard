c2727c1d02fb516733e4e8708758a9d565713384
brisbanebenchtops-commits <brisbanebenchtops@gmail.com>
2025-09-30 21:40:37 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root')
 );
 root.render(<App />);
